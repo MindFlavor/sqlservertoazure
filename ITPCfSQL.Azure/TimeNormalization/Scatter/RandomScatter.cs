4f10d385a544bc8aa0711e76ba17a11454549d9d
Francesco Cogno <francesco.cogno@outlook.com>
2014-06-09 15:29:05 +0200

    Initial commit

---
+++ ITPCfSQL.Azure/TimeNormalization/Scatter/RandomScatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITPCfSQL.Azure.TimeNormalization.Scatter
+{
+    public class RandomScatter
+    {     
+        #region Properties
+        public TimeSpan Step { get; set; }
+        #endregion
+   
+
+        #region Constructors
+        public RandomScatter(
+            TimeSpan tsStep)
+        {
+            this.Step = tsStep;
+        }
+        #endregion
+    }
+}
