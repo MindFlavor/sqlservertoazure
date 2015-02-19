5caca4229b12a932e935c196076c3fa219859433
Francesco Cogno <francesco.cogno@outlook.com>
2015-02-19 17:25:37 +0100

    Merged ScatterTime

---
+++ ITPCfSQL.Azure/TimeNormalization/Scatter/EvenlyDistribute.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITPCfSQL.Azure.TimeNormalization.Scatter
+{
+    public class EvenlyDistribute
+    {
+        #region Members
+        private DateTime dtNextStep = DateTime.MinValue;
+        private List<DateValuePair> lDVP = null;
+        #endregion
+
+        #region Properties
+        public TimeSpan Step { get; set; }
+        #endregion
+
+        #region Constructors
+        public EvenlyDistribute(
+            TimeSpan tsStep)
+        {
+            this.Step = tsStep;
+        }
+        #endregion
+
+        public void Start(DateValuePair Start)
+        {
+            this.dtNextStep = Start.Date.Add(Step);
+            lDVP = new List<DateValuePair>();
+        }
+
+        public List<DateValuePair> Push(DateValuePair dvp)
+        {
+            if (dvp.Date >= dtNextStep)
+            {
+                // output collected
+                double dMSEven = Step.TotalMilliseconds / ((double)lDVP.Count);
+                for (int i = 0; i < lDVP.Count; i++)
+                {
+                    lDVP[i].Date = lDVP[i].Date.AddMilliseconds(dMSEven*i);
+                }
+
+                List<DateValuePair> lDVPOutput = lDVP;
+
+                // reset accumulator
+                lDVP = new List<DateValuePair>();
+                lDVP.Add(dvp);
+
+                // Find next step containing this dvp... this *should* be optimized
+                while (dvp.Date >= (dtNextStep.Add(Step)))
+                {
+                    dtNextStep = dtNextStep.Add(Step);
+                }
+
+                return lDVPOutput;
+            }
+            else
+            {
+                // add
+                lDVP.Add(dvp);
+                // return empty list
+                return new List<DateValuePair>();
+            }
+        }
+    }
+}
---
+++ ITPCfSQL.Azure/TimeNormalization/Scatter/RandomScatter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITPCfSQL.Azure.TimeNormalization.Scatter
+{
+    public class RandomScatter
+    {
+        #region Members
+        public Random _random;
+        #endregion
+
+        #region Properties
+        public TimeSpan Step { get; set; }
+        #endregion
+
+        #region Constructors
+        public RandomScatter(
+            TimeSpan tsStep)
+        {
+            this.Step = tsStep;
+            _random = new Random();
+        }
+        #endregion
+
+        public List<DateValuePair> Push(DateValuePair dvp)
+        {
+            List<DateValuePair> lDVP = new List<DateValuePair>(1);
+            lDVP.Add((DateValuePair)dvp.Clone());
+
+            lDVP[0].Date = lDVP[0].Date.AddMilliseconds(_random.NextDouble() * Step.TotalMilliseconds);
+            return lDVP;
+        }
+    }
+}
